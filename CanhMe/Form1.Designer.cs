69be955c12f7c00e215c7a0b239fb8d5d384a1b0
nonameHDT <nonameHDT@gmail.com>
2016-09-09 20:41:38 +0700

    first commit

---
+++ CanhMe/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CanhMe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.mnuStart = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuStop = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuRestart = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "CanhMe";
+			this.notifyIcon1.Visible = true;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuStart,
+            this.mnuStop,
+            this.mnuRestart,
+            this.mnuExit});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(111, 92);
+			// 
+			// mnuStart
+			// 
+			this.mnuStart.Name = "mnuStart";
+			this.mnuStart.Size = new System.Drawing.Size(110, 22);
+			this.mnuStart.Text = "Start";
+			this.mnuStart.Click += new System.EventHandler(this.mnuStart_Click);
+			// 
+			// mnuStop
+			// 
+			this.mnuStop.Name = "mnuStop";
+			this.mnuStop.Size = new System.Drawing.Size(110, 22);
+			this.mnuStop.Text = "Stop";
+			this.mnuStop.Click += new System.EventHandler(this.mnuStop_Click);
+			// 
+			// mnuRestart
+			// 
+			this.mnuRestart.Name = "mnuRestart";
+			this.mnuRestart.Size = new System.Drawing.Size(110, 22);
+			this.mnuRestart.Text = "Restart";
+			this.mnuRestart.Click += new System.EventHandler(this.mnuRestart_Click);
+			// 
+			// mnuExit
+			// 
+			this.mnuExit.Name = "mnuExit";
+			this.mnuExit.Size = new System.Drawing.Size(110, 22);
+			this.mnuExit.Text = "Exit";
+			this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(573, 308);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.ShowInTaskbar = false;
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuStart;
+        private System.Windows.Forms.ToolStripMenuItem mnuStop;
+        private System.Windows.Forms.ToolStripMenuItem mnuRestart;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.ToolStripMenuItem mnuExit;
+	}
+}
+
---
+++ CanhMe/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace CanhMe
+{
+    public partial class Form1 : Form
+    {
+		List<string> lstAgents = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+			CheckForIllegalCrossThreadCalls = false;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			lstAgents = new List<string>();
+
+			timer1.Interval = 3600000;
+			mnuStart_Click(null, null);
+			timer1_Tick(null, null);
+
+			RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+			rkApp.SetValue("CanhMe", Application.ExecutablePath);
+			rkApp.Close();
+		}
+
+        private void mnuStart_Click(object sender, EventArgs e)
+        {
+			timer1.Start();
+        }
+
+		private void mnuStop_Click(object sender, EventArgs e)
+		{
+			timer1.Stop();
+		}
+
+		private void mnuRestart_Click(object sender, EventArgs e)
+		{
+			mnuStop_Click(null, null);
+			mnuStart_Click(null, null);
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			Thread t = new Thread(Run);
+			t.IsBackground = true;
+			t.Start();
+		}
+
+		private void Run()
+		{
+			Hide();
+			if (File.Exists("agents.txt") && File.Exists("listsites.txt") && File.Exists("folders.txt") && File.Exists("files.txt") && File.Exists("excludes.txt"))
+			{
+				lstAgents.Clear();
+				lstAgents.AddRange(File.ReadAllText("agents.txt").Replace("\r\n", "\n").Split('\n'));
+				List<string> lstSites = new List<string>(File.ReadAllText("listsites.txt").Replace("\r\n", "\n").Split('\n'));
+				List<string> lstFolders = new List<string>(File.ReadAllText("folders.txt").Replace("\r\n", "\n").Split('\n'));
+				List<string> lstFiles = new List<string>(File.ReadAllText("files.txt").Replace("\r\n", "\n").Split('\n'));
+				List<string> lstExcludes = new List<string>(File.ReadAllText("excludes.txt").Replace("\r\n", "\n").Split('\n'));
+				List<string> lstDownloads = new List<string>(File.ReadAllText("downloads.txt").Replace("\r\n", "\n").Split('\n'));
+				
+				List<string> lstLinks = new List<string>();
+
+				foreach (string folder in lstFolders)
+				{
+					if (folder != "")
+						lstLinks.Add(folder + "/");
+				}
+
+				foreach (string file in lstFiles)
+				{
+					if (file != "")
+						lstLinks.Add(file);
+				}
+
+
+				// now make links
+				// and send requests to server
+				foreach (string site in lstSites)
+				{
+					List<string> domainsplit = new List<string>();
+					string temp = site;
+
+					/**** abc.com.vn to abc.com.vn.zip abc.com.zip abc.zip ***/
+					while (temp.LastIndexOf('.') > 0)
+					{
+						int position = temp.LastIndexOf('.');
+						domainsplit.Add(temp.Substring(0, position) + ".zip");
+						domainsplit.Add(temp.Substring(position + 1) + ".zip");
+						temp = temp.Substring(0, position);
+					} 
+					/**************/
+
+
+					string url = MakeUrl(site);
+
+					foreach (string sub in domainsplit)
+					{
+						if (!lstExcludes.Contains(url + sub))
+						{
+							if (CheckUrlExists(url + sub))
+							{
+								MessageBox.Show("Found: " + url + sub);
+							}
+						}
+					}
+
+					foreach (string link in lstLinks)
+					{
+						if (!lstExcludes.Contains(url + link))
+						{
+							if (CheckUrlExists(url + link))
+							{
+								MessageBox.Show("Found: " + url + link);
+							}
+						}
+					}
+
+					foreach (string download in lstDownloads)
+					{
+						if (download != "")
+						{
+							if (!lstExcludes.Contains(url + download))
+							{
+								if (CheckUrlExists(url + download))
+								{
+									MessageBox.Show("Found: " + url + download);
+									WebClient client = new WebClient();
+									client.DownloadFileAsync(new Uri(url + download), site.Replace("\\", "").Replace("/", "") + " " + download);
+								}
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				MessageBox.Show("Missing files.", "!!!!!!!!");
+				Application.Exit();
+			}
+		}
+
+		private string MakeUrl(string url)
+		{
+			url = url.Trim();
+
+			if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+			{
+				url = "http://" + url;
+			}
+			if (!url.EndsWith("/"))
+			{
+				url = url + "/";
+			}
+
+			return url;
+		}
+
+		private bool CheckUrlExists(string URL)
+		{
+			HttpWebRequest request = WebRequest.CreateHttp(URL);
+			request.Timeout = 5000;
+			request.Method = "GET";
+			request.KeepAlive = false;
+			request.AllowAutoRedirect = true;
+			request.UserAgent = lstAgents[new Random().Next(0, lstAgents.Count - 1)];
+
+			HttpWebResponse response = null;
+			try
+			{
+				response = (HttpWebResponse)request.GetResponse();
+
+				if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Found)
+				{
+					StreamWriter w = new StreamWriter("excludes.txt", true);
+					w.WriteLine(URL);
+					w.Close();
+					response.Close();
+					return true;
+				}
+			}
+			catch { }
+			if (response != null)
+			{
+				response.Close();
+			}
+
+			return false;
+		}
+
+		private void mnuExit_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
